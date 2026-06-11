c81071aa95e9909edeff4b0606554bb93e386494
luot-ai <1035768203@qq.com>
2026-06-11 12:52:16 +0800

    [feat] stream-matrix stream-add适配

--- stream-matrix/src/stream-matrix.c
+++ stream-matrix/src/stream-matrix.c
@@ -1,129 +1,8 @@
 #include "trap.h"
+#include "stream_instr.h"
 
 
 //000
-static int cfg_i(int outerIter,int length,int fifo_id)
-{
-    int rs1 = (outerIter & 0xffff) | ((length & 0xffff) << 16);
-    asm volatile (
-       ".insn r 0x0b, 0, 0, x0, %0, %1"
-             :
-             :"r"(rs1),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-static int cfg_i_limit(int limit,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 0, 1, x0, %0, %1"
-             :
-             :"r"(limit),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-static int cfg_i_repeat(int repeat,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 0, 2, x0, %0, %1"
-             :
-             :"r"(repeat),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-
-//001
-// static int cfg_store(uint32_t addr,int fifo_id)
-// {
-//     asm volatile (
-//        ".insn r 0x0b, 1, 0, x0, %0, %1"
-//              :
-//              :"r"(addr),"r"(fifo_id)
-//      );
-//     return 0; 
-// }
-
-//010
-// static int cal_stream(int fifo_id_src0,int fifo_id_src1,int fifo_id_dst)
-// {
-// 	int rs1 = (fifo_id_src0 & 0x3) | ((fifo_id_src1 & 0x3) << 2);
-//     asm volatile (
-//        ".insn r 0x0b, 2, 0, x0, %0, %1"
-//              :
-//              :"r"(rs1),"r"(fifo_id_dst)
-//      );
-//     return 0; 
-// }
-
-//011
-static int cfg_stride(uint32_t stride,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 3, 0, x0, %0, %1"
-             :
-             :"r"(stride),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-//100
-static int cfg_reuse(uint32_t reuse,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 4, 0, x0, %0, %1"
-             :
-             :"r"(reuse),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-//101
-static int cfg_load(uint32_t addr,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 5, 0, x0, %0, %1"
-             :
-             :"r"(addr),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-static int cfg_axi_load(uint32_t addr,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 5, 1, x0, %0, %1"
-             :
-             :"r"(addr),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-//110
-static int cfg_tilestride(uint32_t tilestride,int fifo_id)
-{
-    asm volatile (
-       ".insn r 0x0b, 6, 0, x0, %0, %1"
-             :
-             :"r"(tilestride),"r"(fifo_id)
-     );
-    return 0; 
-}
-
-//111
-static int cal_stream_rd(int fifo_id_src0,int fifo_id_src1)
-{
-    int rd;
-	int rs1 = (fifo_id_src0 & 0x3) | ((fifo_id_src1 & 0x3) << 2);
-    asm volatile (
-       ".insn r 0x0b, 7, 0, %0, %1, x0"
-             :"=r"(rd) 
-             :"r"(rs1)
-     );
-    return rd; 
-}
-
 int test_data[] = {0, 1, 2, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffffffff};
 int ans[] = {0, 0x1, 0x2, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffffffff, 0x1, 0x2, 0x3, 0x80000000, 0x80000001, 0x80000002, 0xffffffff, 0, 0x2, 0x3, 0x4, 0x80000001, 0x80000002, 0x80000003, 0, 0x1, 0x7fffffff, 0x80000000, 0x80000001, 0xfffffffe, 0xffffffff, 0, 0x7ffffffd, 0x7ffffffe, 0x80000000, 0x80000001, 0x80000002, 0xffffffff, 0, 0x1, 0x7ffffffe, 0x7fffffff, 0x80000001, 0x80000002, 0x80000003, 0, 0x1, 0x2, 0x7fffffff, 0x80000000, 0xfffffffe, 0xffffffff, 0, 0x7ffffffd, 0x7ffffffe, 0x7fffffff, 0xfffffffc, 0xfffffffd, 0xffffffff, 0, 0x1, 0x7ffffffe, 0x7fffffff, 0x80000000, 0xfffffffd, 0xfffffffe};
 
@@ -254,7 +133,7 @@ volatile int c[1024];
     for (int i = 0; i< M*N; i++){
         int sum = 0;
         for (int k = 0; k < K;k++){
-            sum += cal_stream_rd(0,1);
+            sum += cal_stream_rd_mul(0,1);
         }
         c[i] = sum;
     }
